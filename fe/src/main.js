3ba8705fcaea44c4066994c1199cc0d6a7002e00
huangyy313 <huangyy313@chinaunicom.cn>
2021-08-31 16:02:50 +0800

    环境配置

--- fe/src/main.js
+++ fe/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-createApp(App).use(router).mount('#app')
+import axios from 'axios'
+import config from './config'
+const app = createApp(App)
+console.log(config);
+axios.get(config.mockApi + 'login').then((res)=>{
+  console.log(res);
+})
+app.use(router).mount('#app')
