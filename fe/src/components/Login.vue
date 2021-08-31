5aa78724f1f776fe73861427ed073f15f4169911
huangyy313 <huangyy313@chinaunicom.cn>
2021-08-31 17:00:50 +0800

    架构设置-axios二次封装

--- fe/src/components/Login.vue
+++ fe/src/components/Login.vue
@@ -3,12 +3,33 @@
   <button @click="goHome">去欢迎页</button>
 </template>
 
-<script setup>
-import { useRouter } from 'vue-router'
-const router = useRouter()
-const goHome = () => {
-  router.push('/')
+<script >
+import Welcome from './Welcome.vue'
+export  default {
+  name:'login',
+  mounted() {
+    this.$request({
+      method:'get',
+      components:{Welcome},
+      url:'login',
+      data:{
+        name:'kelo'
+      }
+    }).then((res)=>{
+      console.log(res)
+    })
+  },
+  methods:{
+    goHome(){
+      this.$router.push('/weclome')
+    }
+  }
 }
+// import { useRouter } from 'vue-router'
+// const router = useRouter()
+// const goHome = () => {
+//   router.push('/')
+// }
 </script>
 
 <style>
--- fe/src/main.js
+++ fe/src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
+import request from './utils/request'
 import config from './config'
 const app = createApp(App)
-console.log(config);
-axios.get(config.mockApi + 'login').then((res)=>{
-  console.log(res);
-})
+app.config.globalProperties.$request = request
 app.use(router).mount('#app')
